02bce0aefb2d667e2ed0c837a8ac5b3662dcc2d0
whileLooper <bochentheone@gmail.com>
2015-06-16 17:28:05 -0400

    upload the readme file

---
+++ Defence of Panda/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public float smooth = 3f;		// a public variable to adjust smoothing of camera motion
+	Transform standardPos;			// the usual position for the camera, specified by a transform in the game
+	Transform lookAtPos;			// the position to move the camera to when using head look
+	
+	void Start()
+	{
+		// initialising references
+		standardPos = GameObject.Find ("CamPos").transform;
+
+	}
+	
+	void FixedUpdate ()
+	{
+
+			// return the camera to standard position and direction
+			transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.deltaTime * smooth);	
+			transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.deltaTime * smooth);
+		
+	}
+}
---
+++ Defence of Panda/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float animSpeed = 1.5f;
+	Vector3 input;
+	Animator anim;
+	bool running = false;
+	public float moveSpeed = 2f;
+
+	static int jumpState = Animator.StringToHash("Base Layer.Jump");
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		//col = GetComponent<CapsuleCollider> ();
+	}
+	
+	void FixedUpdate () {
+		float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		float v = Input.GetAxis("Vertical");				// setup v variables as our vertical input axis
+		anim.SetFloat("Speed", v);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		anim.SetFloat("Direction", h); 						// set our animator's float parameter 'Direction' equal to the horizontal input axis		
+		anim.speed = animSpeed;								// set the speed of our animator to the public variable 'animSpeed'
+		//currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// set our currentState variable to the current state of the Base Layer (0) of animation
+		if (anim.GetFloat("Speed") > 0.1) {
+			running = true;
+		}
+		if (running) {
+			transform.Translate (Vector3.forward * moveSpeed);
+			running = false;
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			anim.SetBool("Jump", true);
+		}
+	}
+}
