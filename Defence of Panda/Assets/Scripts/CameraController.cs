d8767a187acf8253472424d09ace98a2d7615395
Ryan Diaz <rcdiaz123@gmail.com>
2015-06-18 15:16:31 -0400

    Changed camera script
    Camera no longer turns with the player

--- Defence of Panda/Assets/Scripts/CameraController.cs
+++ Defence of Panda/Assets/Scripts/CameraController.cs
@@ -4,22 +4,32 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
 	public float smooth = 3f;		// a public variable to adjust smoothing of camera motion
-	Transform standardPos;			// the usual position for the camera, specified by a transform in the game
-	Transform lookAtPos;			// the position to move the camera to when using head look
-	
+	//Transform standardPos;			// the usual position for the camera, specified by a transform in the game
+	//Transform lookAtPos;			// the position to move the camera to when using head look
+
+	Transform targetPos;
+	Vector3 offset;
+
 	void Start()
 	{
 		// initialising references
-		standardPos = GameObject.Find ("CamPos").transform;
-
+		//standardPos = GameObject.Find ("CamPos").transform;
+		transform.position = GameObject.Find ("CamPos").transform.position;
+		targetPos = GameObject.Find ("Player").transform;
+		offset = targetPos.position - transform.position;
 	}
 	
 	void FixedUpdate ()
 	{
 
-			// return the camera to standard position and direction
-			transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.deltaTime * smooth);	
-			transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.deltaTime * smooth);
+		// return the camera to standard position and direction
+		//standardPos.position = new Vector3 (standardPos.position.x, standardPos.position.y + cameraHeight, 
+		                                    //standardPos.position.z + cameraDistance);
+		//transform.position = Vector3.Lerp(transform.position, new Vector3 (standardPos.position.x, standardPos.position.y + cameraHeight, 
+		                                                                   //standardPos.position.z + cameraDistance), Time.deltaTime * smooth);	
+		//transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.deltaTime * smooth);
 
+		Vector3 targetCameraPos = targetPos.position - offset;
+		transform.position = Vector3.Lerp (transform.position, targetCameraPos, Time.deltaTime * smooth);
 	}
 }
