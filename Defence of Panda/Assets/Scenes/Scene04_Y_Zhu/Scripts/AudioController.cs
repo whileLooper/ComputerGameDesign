6bd605b654aaadb8288bf7139a00fda9903ce8a3
yzhu319 <yuanzhengme@gmail.com>
2015-06-18 21:34:28 -0400

    Add Yuanzheng level scene

---
+++ Defence of Panda/Assets/Scenes/Scene04_Y_Zhu/Scripts/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	private Animator anim;
+	private AudioSource playerFootstepGrass;
+	//private AudioSource playerFootstepWater;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+		playerFootstepGrass = GetComponent<AudioSource>();
+		//playerFootstepWater = audioArray[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.GetFloat("Speed") > 0.01) {
+			playerFootstepGrass.Play ();
+		}
+	}
+}
