e932ca3cf548cd0edec075c8408c8b0f2752ca0e
yzhu319 <yuanzhengme@gmail.com>
2015-06-19 10:08:41 -0400

    Update audio controller script

--- Defence of Panda/Assets/Scenes/Scene04_Y_Zhu/Scripts/AudioController.cs
+++ Defence of Panda/Assets/Scenes/Scene04_Y_Zhu/Scripts/AudioController.cs
@@ -2,24 +2,31 @@
 using System.Collections;
 
 public class AudioController : MonoBehaviour {
-
+	
 	private Animator anim;
-	private AudioSource playerFootstepGrass;
+	private AudioSource[] audioArray;
+	private AudioSource footstepAudio;
+	private AudioSource pushBoxAudio;
 	//private AudioSource playerFootstepWater;
-
-
-	// Use this for initialization
+	
+	
 	void Start () {
 		anim = GetComponent<Animator> ();
-
-		playerFootstepGrass = GetComponent<AudioSource>();
-		//playerFootstepWater = audioArray[1];
+		audioArray = GetComponents<AudioSource>();
+		
+		footstepAudio = audioArray[0];
+		pushBoxAudio = audioArray[1];
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (anim.GetFloat("Speed") > 0.01) {
-			playerFootstepGrass.Play ();
+		if (anim.GetFloat("Speed") > 0.1) {
+			footstepAudio.Play ();
+		}
+		
+		
+		if(anim.GetBool("Push")){
+			pushBoxAudio.Play ();
 		}
 	}
 }
