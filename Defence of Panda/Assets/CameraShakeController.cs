1082ce47f55054b67c11604d3c32ba4f696b0c0e
yzhu319 <yuanzhengme@gmail.com>
2015-07-03 12:58:31 -0400

    Add "earthquake" effect and camera shake
    press Q, enter Kill-them-all mode

---
+++ Defence of Panda/Assets/CameraShakeController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeController : MonoBehaviour {
+
+	GameObject earthquake;
+	ParticleSystem earthquakeParticle;
+
+
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+	
+	// How long the object should shake for.
+	public float shakeDuration = 4.5f;
+	
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.2f;
+	public float decreaseFactor = 1.0f;
+	
+	Vector3 originalPos;
+
+
+
+	void Start () {
+
+
+
+		earthquake = GameObject.FindGameObjectWithTag("Earthquake");
+		earthquakeParticle = earthquake.GetComponent<ParticleSystem>();
+
+
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+			earthquakeParticle.Play();
+			Shake();
+		}
+	}
+
+	void Shake(){
+		StartCoroutine(ShakeCamera());
+	}
+
+	IEnumerator ShakeCamera(){
+		while (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+			Debug.Log(camTransform.localPosition);
+			Debug.Log(shakeDuration);
+			yield return camTransform.localPosition;
+		}
+		Debug.Log(shakeDuration);
+		shakeDuration = 0;
+		camTransform.localPosition = originalPos;
+
+
+	}
+}
