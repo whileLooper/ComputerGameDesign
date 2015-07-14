3365a30884e6faa9b9c25a66e6cc82cfcf06a429
yzhu319 <yuanzhengme@gmail.com>
2015-07-14 17:52:46 -0400

    Spawn enemies at the start point

---
+++ Defence of Panda/Assets/EnemySpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+	
+	public GameObject enemy;
+	public float spawnTime = 5f;
+
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	
+	void Spawn ()
+	{
+//		if(baseHealth.currentHealth <= 0f)
+//		{
+//			return;
+//		}
+
+		Instantiate (enemy, this.transform.position, this.transform.rotation);
+	}
+
+
+}
