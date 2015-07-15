c913530b5e396fb939f4ba6c7c3e37875ff1b0ac
yzhu319 <yuanzhengme@gmail.com>
2015-07-15 11:15:31 -0400

    Add Enemy health bar, enable death animation
    Health bar

--- Defence of Panda/Assets/EnemyHealth.cs
+++ Defence of Panda/Assets/EnemyHealth.cs
@@ -6,18 +6,43 @@ public class EnemyHealth : MonoBehaviour {
 	public float enemyHealth;
 	public float damageEachParticleAttack = 2.0f;
 	Animator anim;
+
+
+	public GameObject healthBar;
+	public GameObject healthBarBase;
+	Vector3 healthBarZeroPoint; //left point of healthbar base
+	float barLengthInit;
+	float barLength;
+
+
 	// Use this for initialization
 	void Awake () {
 		enemyHealth = startingHealth;
 		anim = GetComponent<Animator>();
+		barLengthInit = healthBar.GetComponent<Renderer>().bounds.extents.x; //this is half-length of inital healthbar
+		barLength = barLengthInit;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		//fix orientation of healthBar and healthBarBase
+		healthBar.transform.eulerAngles = new Vector3(90f, 90f, 0);
+		healthBarBase.transform.eulerAngles = new Vector3(90f, 90f, 0);
+
+
+
+		Debug.Log(enemyHealth/startingHealth);
+
+		//shrink health bar length based on currentHealth/startingHealth ratio
+		healthBar.transform.localScale = new Vector3(0.1f,0.5f * Mathf.Clamp(enemyHealth/startingHealth, 0, 1.0f),0.1f);
+		//move healthBar to the left end
+		barLength = enemyHealth/startingHealth * barLengthInit;
+		healthBar.transform.position = healthBarBase.transform.position + new Vector3((barLength - barLengthInit),0,0);
+
 		if(enemyHealth <= 0){
-			anim.SetTrigger ("Dead");
-			Destroy(gameObject);
+			anim.SetTrigger ("Death");
+			//object destroyed in enemy manager script
 		}
 	}
 
@@ -25,5 +50,6 @@ public class EnemyHealth : MonoBehaviour {
 	{
 		Debug.Log("My Enemy's been hit by particles.");
 		enemyHealth -= damageEachParticleAttack;
+
 	}
 }
