d958417fa56398894d9c9e50635836b39ee69f3a
yzhu319 <yuanzhengme@gmail.com>
2015-07-14 23:07:54 -0400

    Add enemy health controller, enemy can die because of particle effect damage

---
+++ Defence of Panda/Assets/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+	public float startingHealth = 100.0f;
+	public float enemyHealth;
+	public float damageEachParticleAttack = 2.0f;
+	Animator anim;
+	// Use this for initialization
+	void Awake () {
+		enemyHealth = startingHealth;
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(enemyHealth <= 0){
+			anim.SetTrigger ("Dead");
+			Destroy(gameObject);
+		}
+	}
+
+	void OnParticleCollision()
+	{
+		Debug.Log("My Enemy's been hit by particles.");
+		enemyHealth -= damageEachParticleAttack;
+	}
+}
