2a7912e4860593b4dd5de67c525e34a95fd8b6f7
yzhu319 <yuanzhengme@gmail.com>
2015-07-15 11:35:13 -0400

    (Minor) change destination point, enemies gather at base's door

--- Defence of Panda/Assets/EnemyNavigation.cs
+++ Defence of Panda/Assets/EnemyNavigation.cs
@@ -5,14 +5,16 @@ public class EnemyNavigation : MonoBehaviour {
 
 	GameObject shack;
 	NavMeshAgent navAgent;
+	Vector3 offset;
 
 	void Awake () {
 		shack = GameObject.FindGameObjectWithTag ("Base");
 		navAgent = GetComponent<NavMeshAgent>();
+		offset = new Vector3(0,0,-1.5f);
 	}
 
 	void Update(){
-		navAgent.SetDestination (shack.transform.position);
+		navAgent.SetDestination (shack.transform.position + offset);
 
 	}
 }
