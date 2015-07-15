200b730ad6bca18fba24253651f993473dbccdba
yzhu319 <yuanzhengme@gmail.com>
2015-07-14 22:04:05 -0400

    Update "PlaceTurret.cs",  place turret as child of haybox

--- Defence of Panda/Assets/Scripts/PlaceTurret.cs
+++ Defence of Panda/Assets/Scripts/PlaceTurret.cs
@@ -11,25 +11,40 @@ public class PlaceTurret : MonoBehaviour {
 	public float rayLength = 1.1f;
 	private RaycastHit hit;
 	private Vector3 turretPos;
+
+	private GameObject turretClone;
+	private GameObject hittedBox;
+
 	// Use this for initialization
 	void Start () {
-		currentTurret = turret2;
+		currentTurret = turret1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("t")) {
+
 			if (Physics.Raycast(transform.position + new Vector3(0f, 0.5f, 0f), transform.forward, out hit, rayLength)) {
 				print (hit.transform.gameObject.tag);
 				if (hit.collider.gameObject.CompareTag("Boxes")) {
-					turretPos = new Vector3(transform.position.x, 0.5f, transform.position.z) + (transform.forward * 1.1f);
-					turretPos.x = Mathf.Round(turretPos.x);
-					turretPos.z = Mathf.Round(turretPos.z);
-					turretPos.y += 1.0f;
-					Instantiate(currentTurret, turretPos, new Quaternion());
+					hittedBox = hit.collider.gameObject;
+
+					//if weapon not built yet, avoid building multiple weapons on top of one box
+					if(hittedBox.transform.childCount == 0){
+						turretPos = new Vector3(transform.position.x, 0.5f, transform.position.z) + (transform.forward * 1.1f);
+						turretPos.x = Mathf.Round(turretPos.x);
+						turretPos.z = Mathf.Round(turretPos.z);
+						turretPos.y += 1.0f;
+
+						//instantiate turret as child object of the corresponding (ray hitted) box
+						turretClone = Instantiate(currentTurret, turretPos, new Quaternion()) as GameObject;
+						turretClone.transform.parent = hittedBox.transform;
+					}
 
 				}
 			}
+
+
 		}
 	}
 }
