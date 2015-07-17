c4e965c35c8aebb8a7adc97fd2dc5f8dcefeab76
Ryan Diaz <rcdiaz123@gmail.com>
2015-07-16 23:18:02 -0400

    Menu Stuff

---
+++ Defence of Panda/Assets/Scripts/Menu Scripts/RollCredits.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RollCredits : MonoBehaviour {
+
+	public Text text;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = text.GetComponent<Animator> ();
+	}
+	void Update() {
+
+	}
+
+	public void creditsRoll() {
+		anim.SetBool ("Credits", true);
+	}
+
+	public void creditsStop() {
+		anim.SetBool ("Credits", false);
+	}
+}
